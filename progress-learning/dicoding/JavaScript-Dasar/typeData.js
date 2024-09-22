ed5d8b6d91f7adac4e72353154abd6abd1770ac4
Dani Akhmad Maulana <121219043+daniAkhmadMaulana@users.noreply.github.com>
2024-09-22 17:55:08 +0700

    Create typeData.js
    The first

---
+++ progress-learning/dicoding/JavaScript-Dasar/typeData.js
@@ -0,0 +1,27 @@
+// Bismillah..
+// In this file I will store my knowledge about type data in JavaScript
+// there are string, number, boolean, and nilai kosong
+
+// String
+const varString1 = 'Ini adalah contoh string';
+const varString2 = "Ini adalah contoh string";
+const varString3 = `Ini adalah contoh string`;
+
+// Number
+const bilBulat = 1;
+const bilDesimal = 10.8;
+
+// Boolean
+const isTrue = true;
+const isFalse = false;
+
+// Nilai kosong (null dan undefined)
+const pesanDuluVar = null;
+let nothingUndefined;
+console.log(typeof(nothingUndefined)); // --> undefined
+
+// Konversi type data
+/*
+  There are many ways to convert type data from variable.
+  String(), .toString(), Number(), parseInt(), parseFloat(), and Boolean().
+*/
