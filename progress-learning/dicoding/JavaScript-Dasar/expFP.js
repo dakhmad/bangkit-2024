abe645807ad669ad2ce25ccfd34ed7de575dde0f
Dani Akhmad Maulana <121219043+daniAkhmadMaulana@users.noreply.github.com>
2024-09-22 17:58:54 +0700

    still salah wkwkw

---
+++ progress-learning/dicoding/JavaScript-Dasar/expFP.js
@@ -0,0 +1,19 @@
+// Example normal
+function pertambahanN (a, b) {
+  let result = a + b;
+  return result;
+}
+
+
+function pertambahanFP (a, b) {
+  return a + b;
+}
+
+console.log(pertambahanN(10,20));
+console.log(pertambahanN(10,20));
+console.log(pertambahanN(10,20));
+
+console.log(pertambahanFP(10,20));
+console.log(pertambahanFP(10,20));
+console.log(pertambahanFP(10,10));
+console.log(pertambahanFP(10,10));
